5f329f2a9848f85c6b1fb206a96ea7e2f018190e
Carlos Valderrama <carlosvg.988@gmail.com>
2024-08-23 12:32:52 -0400

    botones carrito itemcount

---
+++ src/components/ItemCount.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useState } from 'react';
+
+const ItemCount = ({cantidad,handleRestar,handleSumar}) => {
+
+   
+
+
+  return (
+    <div>
+    <div className='item-count'>
+        
+        <button onClick={handleRestar}>-</button>
+        <p>{cantidad}</p>
+        <button onClick={handleSumar}>+</button>
+    </div>
+    <button className='agregar-al-carrito'>Agregar al carrito</button>
+    </div>
+  )
+}
+
+export default ItemCount
--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,8 +1,27 @@
+import React, { useState } from 'react';
+import ItemCount from './ItemCount';
 
 
 
 
 const ItemDetail = ({item}) => {
+
+  const [cantidad,setCantidad] = useState(1);
+
+  const handleRestar = () =>{
+      cantidad >1 && setCantidad(cantidad-1)
+  }
+
+  const handleSumar = ()=>{
+      cantidad < item.stock && setCantidad(cantidad+1)
+
+  }
+
+
+
+
+
+
   return (
     <div className="container">
         <div className="producto-detalle">
@@ -11,9 +30,10 @@ const ItemDetail = ({item}) => {
             <div>
             <h3 className="titulo">{item.titulo}</h3>
             <p className="descripcion">{item.descripcion}</p>
-            <p className="categoria">Categoria:{item.descripción}</p>
-            
+            <p className="categoria">Categoria:{(item.descripción)}</p>
             <p className="precio">${item.precio}</p>
+            <ItemCount cantidad = {cantidad} handleSumar = {handleSumar} handleRestar = {handleRestar}/>
+            
             </div>
 
 
